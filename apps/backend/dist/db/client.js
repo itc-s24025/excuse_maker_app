8b625dd1fa4489ca6892f6fe353011bec3fb3b8f
itc-s24025 <s24025@std.it-college.ac.jp>
2025-12-18 10:49:01 +0900

    動作確認

---
+++ apps/backend/dist/db/client.js
@@ -0,0 +1,7 @@
+export {};
+// import { PrismaClient } from '../prisma/generated/client'
+// import { PrismaPg } from '@prisma/adapter-pg'
+//
+// const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+// export const prisma = new PrismaClient({ adapter })
+//# sourceMappingURL=client.js.map
---
+++ apps/backend/dist/routes/test.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+const router = Router();
+router.get("/hello", (req, res) => {
+    res.json({ status: "バックエンドは働いていますヨーーー！" });
+});
+export default router;
+//# sourceMappingURL=test.js.map
